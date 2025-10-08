3e6de6b1f5b9c0608449707f63d61624f85d8aaf
luis felipe de oliveira <loliveira@inspectos.com>
2025-10-08 18:51:59 -0300

    test: full-stack-changes

--- controlfin-backend/src/server.ts
+++ controlfin-backend/src/server.ts
@@ -242,3 +242,5 @@ if (process.env['NODE_ENV'] !== 'test') {
 // Test comment for full-stack changes
 
 // Test comment for backend changes
+
+// Test comment for full-stack changes
--- controlfin-frontend/src/App.tsx
+++ controlfin-frontend/src/App.tsx
@@ -267,3 +267,5 @@ export default App;
 // Test comment for full-stack changes
 
 // Test comment for frontend changes
+
+// Test comment for full-stack changes
